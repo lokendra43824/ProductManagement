5edb32f5f9bfe1023cf8f0559e74d9267248c007
Lokendra <lokilokendra12345@gmail.com>
2020-11-05 16:48:52 +0530

    [lokendra] first commit

---
+++ Linq/Management.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Linq
+{
+    public class Management
+    {
+        public readonly DataTable data = new DataTable();
+        public void TopThreeRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = (from productReviews in listProductReview
+                                orderby productReviews.Rating descending
+                                select productReviews).Take(5);
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("rating:" + list.Rating + " " + "ID: " + list.ProductId + " " + list.Review);
+                 
+            }
+        }
+
+    } }
+
---
+++ Linq/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Linq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to linq");
+            //uc-1
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ProductId = 1, UserId = 1, Rating = 5, Review = "excellent", isLike = true },
+                 new ProductReview(){ProductId = 2, UserId = 2, Rating = 4, Review = "good", isLike = true },
+                 new ProductReview(){ProductId = 3, UserId = 3, Rating = 3, Review = "average", isLike = true },
+                 new ProductReview(){ProductId = 4, UserId = 4, Rating = 2, Review = "low", isLike = true },
+                 new ProductReview(){ProductId = 5, UserId = 5, Rating = 1, Review = "flop", isLike = true },
+            };
+            //foreach (var list in productReviewList)
+            // {
+            // Console.WriteLine("ProductId:" + list.ProductId + " " + "userId: " + list.UserId);
+            //}
+            Management management = new Management();
+            management.TopThreeRecords(productReviewList);
+        }
+    } }
